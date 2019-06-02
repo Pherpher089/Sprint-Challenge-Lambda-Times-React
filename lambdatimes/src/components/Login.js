cbb31bb056c74529f2d963953b44cca8673d0af6
Pherpher089 <Christutor089@gmail.com>
2019-06-01 17:40:21 -0700

    Added styling to the login page via styled components

--- lambdatimes/src/components/Login.js
+++ lambdatimes/src/components/Login.js
@@ -2,6 +2,36 @@ import React from 'react';
 import styled from 'styled-components'
 
 const LoginDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 15rem;
+    height: 20rem;
+    background-color: white;
+    border: 1px solid lightgrey;
+    margin-left: 40vw;
+    margin-top: 25vh;
+`
+const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`
+const LoginH1 = styled.h1`
+
+`
+const LoginTileDiv = styled.div`
+`
+
+const LoginH2 = styled.h2`
+
+`
+const LoginInput = styled.input`
+    margin-top: 1rem;
+`
+const LoginButton = styled.button`
+    margin-top: 1rem;   
+
 `
 
 class Login extends React.Component{
@@ -12,18 +42,23 @@ class Login extends React.Component{
     render(){
         return(
             <LoginDiv>
-                <form onSubmit={this.props.onSubmit}>
-                    <input 
+                <LoginForm onSubmit={this.props.onSubmit}>
+                    <LoginTileDiv>
+                        <LoginH1>Lambda Times</LoginH1>
+                        <LoginH2>Log in</LoginH2>
+                    </LoginTileDiv>
+                    
+                    <LoginInput 
                         type='text'
                         placeHolder='username' name='username'
                         value={this.props.userValue} onChange={this.props.changeHandler}>
-                    </input>
-                    <input 
+                    </LoginInput>
+                    <LoginInput 
                         type='password' placeHolder='password' name='password' 
                         value={this.props.passValue} onChange={this.props.changeHandler}>
-                    </input>
-                    <button>Log in</button>
-                </form>
+                    </LoginInput>
+                    <LoginButton>Log in</LoginButton>
+                </LoginForm>
             </LoginDiv>
         )
     }
